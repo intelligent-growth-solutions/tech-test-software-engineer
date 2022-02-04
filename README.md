bf82968ffb1f259c0090621d84f38c1e5cc82dd8
Adrian Hristov <adrian@intelligentgrowthsolutions.com>
2022-02-04 16:23:23 +0000

    Added initial API and WIP of readme

---
+++ README.md
@@ -0,0 +1,56 @@
+# IGS Software Engineer Tech Test
+One of the core functions of the IGS software is to automate the growing of plants. Our plant scientists come up with come up with **Recipes** that are a sort of a blueprint for how plants should be grown - when to expose them to light, when to water them etc.
+
+The growing process happens in, what we call, a Tower. An IGS Tower has a number of trays, stacked vertically. Each tray is equipped with lights and has the ability to be watered.
+
+# Objective
+Given the list of recipe names, a start dates and a tray numbers outlined below, create an application that generates a JSON file with a schedule. The schedule should outline at exactly what time light and water commands should be sent to the Tower, and what light intensity or amount to water should be.
+
+```typescript
+{
+  input: [
+    {
+      trayNumber: 1,
+      recipeName: "Basil",
+      startDate: "2022-01-24T12:30:00.0000000Z"
+    },
+    {
+      trayNumber: 2,
+      recipeName: "Strawberries",
+      startDate: "2021-13-08T17:33:00.0000000Z"
+    },
+    {
+      trayNumber: 3,
+      recipeName: "Basil",
+      startDate: "2030-01-01T23:45:00.0000000Z"
+    }
+  ]
+}
+```
+
+# Recipes
+In an IGS tower, we control all weather conditions normally associated with the growth process. Because we control the light, we don't have the concept of days in a recipe, but rather a set of Phases.
+
+A simple example would be a Phase that is 24 hours long and is repeated 7 times. This would translate into exactly 1 week. Phases however, can be as many hours as is specified in the recipe so you could have 36 hour phases where lights are on for longer, effectively making each "day" longer.
+
+Within each recipe there can be 1 or more phases. This is to optimise the amount of light and water a plant needs for optimal growth.
+
+## Lighting Phase
+Each lighting phase contains 1 or more Operations.
+
+Each Operation specifies an offset time for when it should be executed relative to the beginning of the phase. It also specifies the intensity.
+
+Light intensity is represented by a number 0-3:
+Number | Intensity
+------ | --------
+0      | Off
+1      | Low
+2      | Medium
+3      | High
+
+# Watering Phase
+Each watering phase contains 0 or more Operations.
+
+Each Operation specifies an offset
+
+
---
+++ RecipeApi/Program.cs
@@ -0,0 +1,72 @@
+using IgsSoftwareTechTest;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/recipe", () =>
+{
+    var recipe1 = new Recipe("Basil");
+    recipe1.LightingPhases.Add(new LightingPhase("LightingPhase 1", 0, 24, 0, 5)
+    {
+        Operations = new List<LightingPhaseOperation>
+        {
+            new LightingPhaseOperation(0, 0, LightIntensity.Low),
+            new LightingPhaseOperation(6, 0, LightIntensity.Medium),
+            new LightingPhaseOperation(12, 0, LightIntensity.High),
+            new LightingPhaseOperation(16, 0, LightIntensity.Off),
+        }
+    });
+    
+    recipe1.WateringPhases.Add(
+        new WateringPhase("Watering Phase 1", 0, 24, 0, 5, 100));
+
+    var recipe2 = new Recipe("Strawberries");
+    recipe2.LightingPhases.Add(new LightingPhase("Phase 3", 0, 24, 0, 5)
+    {
+        Operations = new List<LightingPhaseOperation>
+        {
+            new LightingPhaseOperation(0, 0, LightIntensity.High),
+            new LightingPhaseOperation(20, 0, LightIntensity.Off),
+        }
+    });
+    recipe2.LightingPhases.Add(new LightingPhase("Phase 2", 1, 36, 30, 10)
+    {
+        Operations = new List<LightingPhaseOperation>
+        {
+            new LightingPhaseOperation(0, 0, LightIntensity.Low),
+            new LightingPhaseOperation(6, 0, LightIntensity.Medium),
+            new LightingPhaseOperation(12, 0, LightIntensity.High),
+            new LightingPhaseOperation(16, 30, LightIntensity.Medium),
+            new LightingPhaseOperation(24, 30, LightIntensity.Low),
+            new LightingPhaseOperation(30, 0, LightIntensity.Off),
+        }
+    });
+    recipe2.LightingPhases.Add(new LightingPhase("Phase 3", 0, 24, 0, 2)
+    {
+        Operations = new List<LightingPhaseOperation>
+        {
+            new LightingPhaseOperation(0, 0, LightIntensity.Low),
+            new LightingPhaseOperation(6, 0, LightIntensity.Medium),
+            new LightingPhaseOperation(12, 0, LightIntensity.Off),
+        }
+    });
+    recipe2.WateringPhases.Add(
+        new WateringPhase("Phase 1", 0, 24, 0, 5, 0));
+    recipe2.WateringPhases.Add(
+        new WateringPhase("Phase 2", 1, 24, 0, 6, 55));
+    recipe2.WateringPhases.Add(
+        new WateringPhase("Phase 2", 3, 24, 0, 2, 30));
+    recipe2.WateringPhases.Add(
+        new WateringPhase("Phase 2", 2, 12, 30, 4, 30));
+
+    var recipes = new List<Recipe>
+    {
+        recipe1, recipe2
+    };
+
+    var rootObject = new { recipes = recipes };
+    
+    return Results.Ok(rootObject);
+});
+
+app.Run();
